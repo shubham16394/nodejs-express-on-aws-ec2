6edf09ae26cccba99f739afc3381cbfcab862d21
Shubham Tiwari <shubham16394@gmail.com>
2022-11-09 22:32:17 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -11,6 +11,10 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
+nvm ls
+node -v
+npm -v
+
 #install node modules
 npm install
 
