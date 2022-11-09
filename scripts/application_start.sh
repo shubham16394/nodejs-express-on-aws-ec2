62be378ab92621ceb9a64616cc4dbed160226236
Shubham Tiwari <shubham16394@gmail.com>
2022-11-09 21:39:19 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -11,6 +11,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
+nvm use --delete-prefix v19.0.1
+
 nvm ls
 nvm install 16
 nvm use 16
