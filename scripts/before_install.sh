b8ce1d8095b9d207c27e9dba49881696519cd072
Shubham Tiwari <shubham16394@gmail.com>
2022-11-09 22:36:46 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 #download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 14
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+# . ~/.nvm/nvm.sh
+# nvm install 14
 
-#create our working directory if it doesnt exist
-DIR="/home/ec2-user/express-app"
-if [ -d "$DIR" ]; then
-  echo "${DIR} exists"
-else
-  echo "Creating ${DIR} directory"
-  mkdir ${DIR}
-fi
+# #create our working directory if it doesnt exist
+# DIR="/home/ec2-user/express-app"
+# if [ -d "$DIR" ]; then
+#   echo "${DIR} exists"
+# else
+#   echo "Creating ${DIR} directory"
+#   mkdir ${DIR}
+# fi
